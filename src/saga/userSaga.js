6e6efcc3969de77b1bb586b820d56b8f38182e42
slava-azdevelopment <slava@azdevelopment.net>
2023-11-05 23:30:23 +0200

    first commit

--- src/saga/userSaga.js
+++ src/saga/userSaga.js
@@ -1,9 +1,10 @@
+import {addCashAction} from "../store/cashReducer";
 
 const delay = (ms) => new Promise(res => setTimeout(res, ms));
 
 function* addCash() {
 	yield delay(1000);
-	yield put(addCash());
+	yield put(addCashAction());
 }
 
 function* deleteCash() {
